bab4e0e72e4997974730a1b961bda39c2f7658c5
Kevin Thompson <mrunleaded@gmail.com>
2009-10-02 09:17:22 -0700

    Wrote a TestApp to check the lib.
    Signed-off-by: Kevin Thompson <mrunleaded@gmail.com>

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtUri = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(205, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(205, 53);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtUri
+            // 
+            this.txtUri.Location = new System.Drawing.Point(12, 15);
+            this.txtUri.Name = "txtUri";
+            this.txtUri.Size = new System.Drawing.Size(187, 20);
+            this.txtUri.TabIndex = 2;
+            this.txtUri.Text = "udp://172.16.0.2@:20000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.txtUri);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtUri;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using RtpLib;
+using Action=System.Action;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        private RtpListener _listener;
+        private FileStream _file;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("temp.mpv"))
+                File.Delete("temp.mpv");
+
+            _file = new FileStream("temp.mpv", FileMode.CreateNew);
+
+            _listener = RtpListener.Open(txtUri.Text);
+            _listener.SequencedMarkerReceived += MarkerReceived;
+            _listener.VerifyPayloadType = false;
+        }
+
+        void MarkerReceived(object sender, EventArgs<RtpPacket> e)
+        {
+            var data = _listener.GetCombinedPayload();
+            _file.Write(data, 0, data.Length);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (_listener == null) return;
+            _listener.StopListening();
+            _listener.Dispose();
+            _listener = null;
+            _file.Close();
+            _file = null;
+        }
+    }
+}
